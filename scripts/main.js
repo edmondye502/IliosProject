f551134d8f0c5fae3c0a0f3a0de3eb892fca8d56
meltonb <brentmelton99@yahoo.com>
2015-02-19 20:11:30 -0800

    Text Now Appears
    Text reflects the full path, but isn't angled very nicely. Added
    competencies to data load, but haven't used them yet. Attempt at
    3-layer, shown in comments.

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,15 @@ require([
     'data/programYears',
     'data/cohorts',
     'data/courses',
+    'data/competencies',
     'domready'
 ], 
-function(programs, programYears, cohorts, courses,  domReady) 
+function(programs, programYears, cohorts, courses, competencies, domReady) 
 {
 	var draw = function()
 	{
 
-		var width = 960,
+		var width = 860,
 	    height = 700,
 	    radius = Math.min(width, height) / 2;
 
@@ -62,6 +63,30 @@ function(programs, programYears, cohorts, courses,  domReady)
 		};
 
 
+		// for(var p in programs){
+		// 	var individual_p = programs[p]; // ["PH","M"]
+		// 	var program_year_ids = individual_p.programYears; // ['58','69'] contains indicies
+		// 	var py_branch = [];
+
+		// 	// use individual indicies to get program years
+		// 	for (var i = 0; i < program_year_ids.length; i++) {
+		// 		p_id = program_year_ids[i];
+
+		// 		// use p_index to get that program year's array of competency indicies
+		// 		competency_ids = programYears[p_id].competencies;
+		// 		var c_titles = [];
+
+		// 		// use competency_ids to get individual competency titles
+		// 		for (var i = 0; i < competency_ids.length; i++) {
+		// 			c_id = competency_ids[i];
+		// 			c_titles.push(competencies[c_id].title);
+		// 		};
+		// 		py_branch.push({title: programYears[p_id].id, children: c_titles});
+		// 	};
+		// 	current_layer.push({title: individual_p.title, children: py_branch});
+		// };
+
+
 
 		var pathObj = {title: "Root", children: current_layer}; // the full path
 
@@ -72,119 +97,65 @@ function(programs, programYears, cohorts, courses,  domReady)
 		
 		node = root;
 
+		// original
 		// draw the sections of the sunburst using the path
-		var path = svg.datum(root).selectAll("path")
-		  	.data(partition.nodes).enter()
-		 	.append("path")
-		    .attr("id", function(d, i) { return "path-" + i; })
+		// var path = svg.datum(root).selectAll("path")
+		//   	.data(partition.nodes).enter()
+		//  	.append("path")
+		//     .attr("id", function(d, i) { return "path-" + i; })
+	    // .attr("d", arc)
+		//  .style("fill", function(d) { return color((d.children ? d : d.parent).title); })
+		//  .on("click", click)
+		//  .each(stash);
 
+		var g = svg.selectAll("g")
+		      .data(partition.nodes(root))
+		    .enter().append("g");
+
+	
+
+		  // new
+		  var path = g.append("path")
 		    .attr("d", arc)
 		    .style("fill", function(d) { return color((d.children ? d : d.parent).title); })
-		    .on("click", click)
-		    .each(stash);
+		    .on("click", click);
 
-		// Used to switch between "size" and "count" buttons
-		//d3.selectAll("input").on("change", function change() {
-		//  var value = this.value === "count"
-		//      ? function() { return 1; }
-		//      : function(d) { return d.size; };
-		//
-		//  path
-		//      .data(partition.value(value).nodes)
-		//      .transition()
-		//      .duration(1000)
-		//      .attrTween("d", arcTweenData);
-		//});
-		  
-		// var text = vis.selectAll("text").data(nodes);
-		// var textEnter = text.enter().append("text")
-		//     .style("fill-opacity", 1)
-		//     .style("fill", function(d) {
-		//       return brightness(d3.rgb(colour(d))) < 125 ? "#eee" : "#000";
-		//     })
-		//     .attr("text-anchor", function(d) {
-		//       return x(d.x + d.dx / 2) > Math.PI ? "end" : "start";
-		//     })
-		//     .attr("dy", ".2em")
-		//     .attr("transform", function(d) {
-		//       var multiline = (d.name || "").split(" ").length > 1,
-		//           angle = x(d.x + d.dx / 2) * 180 / Math.PI - 90,
-		//           rotate = angle + (multiline ? -.5 : 0);
-		//       return "rotate(" + rotate + ")translate(" + (y(d.y) + padding) + ")rotate(" + (angle > 90 ? -180 : 0) + ")";
-		//     })
-		//     .on("click", click);
-		// textEnter.append("tspan")
-		//     .attr("x", 0)
-		//     .text(function(d) { return d.depth ? d.name.split(" ")[0] : ""; });
-		// textEnter.append("tspan")
-		//     .attr("x", 0)
-		//     .attr("dy", "1em")
-		//     .text(function(d) { return d.depth ? d.name.split(" ")[1] || "" : ""; });
 
-		  function click(d) {
-		    node = d;
-		    console.log(current_layer);
-		    console.log(d.title);
-		    path.transition()
-		      .duration(1000)
-		      .attrTween("d", arcTweenZoom(d));
-		      // text.style("visibility", function(e) {
-		      //     return isParentOf(d, e) ? null : d3.select(this).style("visibility");
-		      //   })
-		      // .transition()
-		      //   .duration(duration)
-		      //   .attrTween("text-anchor", function(d) {
-		      //     return function() {
-		      //       return x(d.x + d.dx / 2) > Math.PI ? "end" : "start";
-		      //     };
-		      //   })
-		      //   .attrTween("transform", function(d) {
-		      //     var multiline = (d.name || "").split(" ").length > 1;
-		      //     return function() {
-		      //       var angle = x(d.x + d.dx / 2) * 180 / Math.PI - 90,
-		      //           rotate = angle + (multiline ? -.5 : 0);
-		      //       return "rotate(" + rotate + ")translate(" + (y(d.y) + padding) + ")rotate(" + (angle > 90 ? -180 : 0) + ")";
-		      //     };
-		      //   })
-		      //   .style("fill-opacity", function(e) { return isParentOf(d, e) ? 1 : 1e-6; })
-		      //   .each("end", function(e) {
-		      //     d3.select(this).style("visibility", isParentOf(d, e) ? null : "hidden");
-		      //   });
-		  }
+		  var text = g.append("text")
+		    .attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; })
+		    .attr("x", function(d) { return y(d.y); })
+		    .attr("dx", "6") // margin
+		    .attr("dy", ".35em") // vertical-align
+		    .text(function(d) { return d.title; });
+
+		    function click(d) 
+		    	{
+			    // fade out all text elements
+			    text.transition().attr("opacity", 0);
+
+			    path.transition()
+			      .duration(750)
+			      .attrTween("d", arcTween(d))
+			      .each("end", function(e, i) {
+	         	 // check if the animated element's data e lies within the visible angle span given in d
+	         	if (e.x >= d.x && e.x < (d.x + d.dx)) {
+	            // get a selection of the associated text element
+	            var arcText = d3.select(this.parentNode).select("text");
+	            // fade in the text element and recalculate positions
+	            arcText.transition().duration(750)
+	              .attr("opacity", 1)
+	              .attr("transform", function() { return "rotate(" + computeTextRotation(e) + ")" })
+	              .attr("x", function(d) { return y(d.y); });
+          }
+      });
+  }
 		
 
 		d3.select(self.frameElement).style("height", height + "px");
 
-		// Setup for switching data: stash the old values for transition.
-		function stash(d) {
-		  d.x0 = d.x;
-		  d.dx0 = d.dx;
-		}
 
-		// When switching data: interpolate the arcs in data space.
-		function arcTweenData(a, i) {
-		  var oi = d3.interpolate({x: a.x0, dx: a.dx0}, a);
-		  function tween(t) {
-		    var b = oi(t);
-		    a.x0 = b.x;
-		    a.dx0 = b.dx;
-		    return arc(b);
-		  }
-		  if (i == 0) {
-		   // If we are on the first arc, adjust the x domain to match the root node
-		   // at the current zoom level. (We only need to do this once.)
-		    var xd = d3.interpolate(x.domain(), [node.x, node.x + node.dx]);
-		    return function(t) {
-		      x.domain(xd(t));
-		      return tween(t);
-		    };
-		  } else {
-		    return tween;
-		  }
-		}
-
-		// When zooming: interpolate the scales.
-		function arcTweenZoom(d) {
+		// Interpolate the scales!
+		function arcTween(d) {
 		  var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
 		      yd = d3.interpolate(y.domain(), [d.y, 1]),
 		      yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
@@ -193,6 +164,10 @@ function(programs, programYears, cohorts, courses,  domReady)
 		        ? function(t) { return arc(d); }
 		        : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
 		  };
+		}
+
+		function computeTextRotation(d) {
+		  return (x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180;
 		}
 	};
 
